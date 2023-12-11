986adc201b622b5ec22e123f1e080f5cca7d351a
Appz-dk <patrick.fyhn@gmail.com>
2023-12-11 01:16:04 +0100

    feat: added data file

---
+++ src/data/mainData.ts
@@ -0,0 +1,18 @@
+export const categories = [
+  "All",
+  "JavaScript",
+  "TypeScript",
+  "Vue",
+  "Programming",
+  "Data Structures",
+  "Football",
+  "Hiking",
+  "React",
+  "Next.js",
+  "Functional Programming",
+  "Object Oriented Programming",
+  "Frontend Web Development",
+  "Backend Web Development",
+  "Web Development",
+  "Coding",
+]
