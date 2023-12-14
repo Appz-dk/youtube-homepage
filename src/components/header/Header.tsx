22db2fe36e08bd940f4bac1691b2ccd5fd22b190
Appz-dk <patrick.fyhn@gmail.com>
2023-12-14 17:48:09 +0100

    clean-up: Removed unused code

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Bell, Menu, Mic, Search, Upload, User } from "lucide-react"
+import { Bell, Mic, Search, Upload, User } from "lucide-react"
 
 import { Button } from "../utils/Button"
 import { Searchbar } from "./Searchbar"
