73482f776ad4b1e950201a9f018d4e2f8be220de
Appz-dk <patrick.fyhn@gmail.com>
2023-12-14 02:33:46 +0100

    feat: added sidebar context provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@ import './App.css'
 import { Header } from './components/header/Header'
 import { MainContent } from './components/mainContent/MainContent'
 import { Sidebar } from './components/sidebar/Sidebar'
+import { SidebarProvider } from './context/SidebarContext'
 
 function App() {
 
   return (
-    <div className='max-h-screen flex flex-col'>
-      <Header />
-      <div className='grid grid-cols-[auto,1fr] flex-grow overflow-auto'>
-        <Sidebar />
-        <div className='overflow-x-hidden px-8 pb-4'>
-          <MainContent />
+    <SidebarProvider>
+      <div className='max-h-screen flex flex-col'>
+        <Header />
+        <div className='grid grid-cols-[auto,1fr] flex-grow overflow-auto'>
+          <Sidebar />
+          <div className='overflow-x-hidden px-8 pb-4'>
+            <MainContent />
+          </div>
         </div>
       </div>
-    </div>
+    </SidebarProvider>
   )
 }
 
